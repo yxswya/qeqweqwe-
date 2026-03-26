eca998f52eb3faa70cedc8fdd23e416e002c8822
@油腻_w4v0 <16615812538@163.com>
2026-03-26 10:08:50 +0800

    change

--- src/api/modules/session.ts
+++ src/api/modules/session.ts
@@ -3,7 +3,7 @@ import { treaty } from '@elysiajs/eden'
 import { getAccessToken } from '@/auth.ts'
 
 // @ts-ignore
-export const server = treaty<App>('localhost:3002', {
+export const server = treaty<App>('http://101.35.246.159:3010', {
   headers: {
     Authorization: `Bearer ${getAccessToken()}`,
   },
--- src/components/Session/WorkFlow/WorkFlow/Products.tsx
+++ src/components/Session/WorkFlow/WorkFlow/Products.tsx
@@ -25,7 +25,7 @@ const Products: React.FC = () => {
         {rags?.map((el, index) => (
           <Link
             key={el.id}
-            to={`/rag-answer/${el.indexVersion}`}
+            to={`/rag-answer/${el.indexVersion}/${sessionId}`}
             className="flex items-center gap-2.5 p-2.5 rounded-lg hover:bg-blue-50 group transition-colors duration-150"
           >
             <span className="shrink-0 flex items-center justify-center w-5 h-5 text-xs font-semibold text-white bg-blue-500 rounded-full group-hover:bg-blue-600 transition-colors">
